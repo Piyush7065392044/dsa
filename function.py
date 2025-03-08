25c16dab22b4c1a484deb31696e8ed15bab07dfd
Piyush7065392044 <129537005+Piyush7065392044@users.noreply.github.com>
2025-03-09 01:36:06 +0530

    functions

---
+++ function.py
@@ -0,0 +1,43 @@
+# fucntion and 
+# def greet():
+#     name = input("enter name")
+#     print("Hello, " +name)
+
+# greet()
+# two sum fucntion 
+# def twosum (a,b):
+#     return  a+b
+# a= int(input("enter value ")) 
+# b= int (input("enter value "))
+# result = twosum(a , b )
+# print(result)
+
+# fucntion using if else 
+
+# def even_odd(number ):
+#     if number % 2  == 0 :
+#         return "even"
+#     else : 
+#         return "odd"
+# number = int( input("enter the value "))
+# num =  even_odd(number)
+
+# print(num)
+
+
+
+def loop (a):
+    return a 
+
+i = int(input("enter the value of a under 100 "))
+a = int(input("enter the value of a under 100 "))
+for a in range (100):
+   
+    if  a <=i :
+       
+        print(a)
+    
+    else :
+        continue
+hello = loop(a)
+print(hello)
